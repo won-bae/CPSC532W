3c220a0297f3da839ddc338576fe8192fd0b9f25
won-bae <bwh0324@gmail.com>
2021-10-17 16:20:22 -0400

    added json load

--- HW2/evaluation_based_sampling.py
+++ HW2/evaluation_based_sampling.py
@@ -3,6 +3,7 @@ from daphne import daphne
 from tests import is_tol, run_prob_test,load_truth
 from primitives import Primitives
 from collections.abc import Hashable
+import json
 
 
 primitives = Primitives()
@@ -74,13 +75,14 @@ def get_stream(ast):
     """Return a stream of prior samples"""
     while True:
         yield evaluate_program(ast)
-    
+
 
 
 def run_deterministic_tests():
     for i in range(1,14):
         #note: this path should be with respect to the daphne path!
-        ast = daphne(['desugar', '-i', '../programs/tests/deterministic/test_{}.daphne'.format(i)])
+        #ast = daphne(['desugar', '-i', '../programs/tests/deterministic/test_{}.daphne'.format(i)])
+        ast = json.load('json/deterministic/test_{}.json'.format(i))
         print("{}th test ast: {}".format(i, ast))
         truth = load_truth('programs/tests/deterministic/test_{}.truth'.format(i))
         ret, sig = evaluate_program(ast)
@@ -88,43 +90,44 @@ def run_deterministic_tests():
             assert(is_tol(ret, truth))
         except AssertionError:
             raise AssertionError('return value {} is not equal to truth {} for exp {}'.format(ret,truth,ast))
-        
+
         print('Test passed')
-        
+
     print('All deterministic tests passed')
-    
+
 
 
 def run_probabilistic_tests():
-    
+
     num_samples=1e4
     max_p_value = 1e-4
-    
+
     for i in range(1,7):
-        #note: this path should be with respect to the daphne path!        
+        #note: this path should be with respect to the daphne path!
         ast = daphne(['desugar', '-i', '../programs/tests/probabilistic/test_{}.daphne'.format(i)])
         print("{}th test ast: {}".format(i, ast))
         truth = load_truth('programs/tests/probabilistic/test_{}.truth'.format(i))
-        
+
         stream = get_stream(ast)
-        
+
         p_val = run_prob_test(stream, truth, num_samples)
-        
+
         print('p value', p_val)
         assert(p_val > max_p_value)
-    
-    print('All probabilistic tests passed')    
 
-        
+    print('All probabilistic tests passed')
+
+
 if __name__ == '__main__':
 
     run_deterministic_tests()
-    
+
     run_probabilistic_tests()
 
 
     for i in range(1,5):
-        ast = daphne(['desugar', '-i', '../programs/{}.daphne'.format(i)])
+        #ast = daphne(['desugar', '-i', '../programs/{}.daphne'.format(i)])
+        ast = json.load('json/{}.json'.format(i))
         print('\n\n\nSample of prior of program {}:'.format(i))
         print("{}th test ast: {}".format(i, ast))
         result = evaluate_program(ast)
